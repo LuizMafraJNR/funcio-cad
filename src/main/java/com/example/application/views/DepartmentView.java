768f224faf946029e44caf11280c48010f8039dc
Luiz Mafra <luiz.junior@neomind.com.br>
2023-09-29 07:56:46 -0300

    Novos ajustes

--- src/main/java/com/example/application/views/DepartmentView.java
+++ src/main/java/com/example/application/views/DepartmentView.java
@@ -2,7 +2,11 @@ package com.example.application.views;
 
 import com.example.application.component.data.DepartamentoDataProvider;
 import com.example.application.dto.DepartamentoDTO;
+import com.example.application.dto.FuncionarioDTO;
+import com.vaadin.flow.component.Html;
 import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.button.ButtonVariant;
+import com.vaadin.flow.component.crud.Crud;
 import com.vaadin.flow.component.grid.Grid;
 import com.vaadin.flow.component.html.Div;
 import com.vaadin.flow.component.icon.Icon;
@@ -30,12 +34,27 @@ public class DepartmentView extends Div {
         grid.setMinWidth("97vw");
         grid.setMinHeight("400px");
 
-        Button adicionaDepartamento = new Button("Adicionar Departamento", VaadinIcon.PLUS.create());
-        HorizontalLayout footer = new HorizontalLayout(adicionaDepartamento);
-        footer.setAlignItems(FlexComponent.Alignment.CENTER);
-        footer.getThemeList().clear();
+//        Button adicionaDepartamento = new Button("Adicionar Departamento", VaadinIcon.PLUS.create());
+//        HorizontalLayout footer = new HorizontalLayout(adicionaDepartamento);
+//        footer.setAlignItems(FlexComponent.Alignment.AUTO);
+//        footer.getStyle().set("flex-wrap", "wrap");
+//        footer.getThemeList().clear();
 
-        add(grid,footer);
+
+        Button button = new Button("Novo Departamento", VaadinIcon.PLUS.create());
+        button.addThemeVariants(ButtonVariant.LUMO_TERTIARY);
+
+        HorizontalLayout toolbar = new HorizontalLayout();
+        toolbar.setAlignItems(FlexComponent.Alignment.CENTER);
+        toolbar.setFlexGrow(1, button);
+        toolbar.setSpacing(false);
+
+        add(grid,button);
+    }
+
+    private void setupToolbar() {
+
+
     }
 
     private Icon createStatusIcon(DepartamentoDTO departamentoDTO){
